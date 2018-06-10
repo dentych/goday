efdc2a1d8f8281bbb6379f9bdee19581753525d4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2018-06-10 11:33:21 +0200

    Created database project with ef core

---
+++ database/GDB/GDB.Database/GoContext.cs
@@ -0,0 +1,18 @@
+﻿using GDB.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GDB.Database
+{
+    public class GoContext : DbContext
+    {
+        public DbSet<GoFile> GoFiles { get; set; }
+
+        public GoContext(DbContextOptions options) : base(options) { }
+        public GoContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Go;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ database/GDB/GDB.Model/GoFile.cs
@@ -0,0 +1,9 @@
+﻿namespace GDB.Model
+{
+    public class GoFile
+    {
+        public int Id { get; set; }
+        public string Hash { get; set; }
+        public string Location { get; set; }
+    }
+}
