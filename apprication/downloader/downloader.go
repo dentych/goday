0249ff6c858dd57fed9d79263cc84bb9cdc67cf0
dentych <dennis@tychsen.me>
2018-06-10 15:21:30 +0200

    Run gofmt on files

--- apprication/downloader/downloader.go
+++ apprication/downloader/downloader.go
@@ -9,16 +9,15 @@ func Hello() {
 	fmt.Println("Hello!")
 }
 
-
 // Will download a file from an FTP server, specified by addr. Addr should contain a hostname and a port number.
 // Filepath path and filename of the file to download.
 // The method returns a Response which can be read for the contents of the downloaded file. The Response MUST BE closed
 // after use, otherwise FTP errors might happen.
-func DownloadFileAnonymously(addr, filePath string) (*ftp.Response){
+func DownloadFileAnonymously(addr, filePath string) *ftp.Response {
 	return DownloadFile(addr, "anonymous", "anonymous", filePath)
 }
 
-func DownloadFile(addr, username, password, filePath string) (*ftp.Response) {
+func DownloadFile(addr, username, password, filePath string) *ftp.Response {
 	con, err := ftp.Connect(addr)
 	if err != nil {
 		panic(err.Error())
