feba003a187071b6da101b6c20ccad5de12242f2
dentych <dennis@tychsen.me>
2018-06-10 11:47:43 +0200

    Such rename, very files

---
+++ apprication/downloader/downloader.go
@@ -0,0 +1,7 @@
+package downloader
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello!");
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/goday/apprication/downloader"
+
+func main() {
+	downloader.Hello()
+}
