2de8a0db1a1bee3f1bd678125a7d1afa1779e596
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2018-06-10 15:12:06 +0200

    Minor data stuff

--- james/james.go
+++ james/james.go
@@ -1,6 +1,7 @@
 package james
 
 import (
+	"database/sql"
 	"fmt"
 )
 
@@ -12,6 +13,15 @@ type GoFile struct {
 
 // Save persists strings hash and location to mssql database
 func Save(hash, location string) bool {
+	db, err := sql.Open("mssql", "localhost")
+	if err != nil {
+		fmt.Println("Unable to connect to db", err)
+	}
+	rows := db.QueryRow("SELECT * FROM dbo.GoFiles")
+
+	file := new(GoFile)
+	rows.Scan(&file.hash, &file.location)
+
 	fmt.Println(hash, location)
 	return false
 }
