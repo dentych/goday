d2e4af64698f7d1cfd2aecbb24d419568fe22f3d
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2018-06-10 17:03:31 +0200

    Added comments to downloader and started the api

--- apprication/api/api.go
+++ apprication/api/api.go
@@ -1,3 +1,35 @@
-package api
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"github.com/dentych/goday/apprication/downloader"
+	"github.com/gorilla/mux"
+)
+
+type postBody struct {
+	Host     string
+	Path     string
+	Username string
+	Password string
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", post)
+	http.ListenAndServe(":8080", router)
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var body postBody
+	err := decoder.Decode(&body)
+
+	if err != nil {
+		fmt.Println("Decode failed:", err)
+	}
+
+	downloader.DownloadFile(body.Host, body.Username, body.Username, body.Password)
+}
--- apprication/downloader/downloader.go
+++ apprication/downloader/downloader.go
@@ -1,15 +1,10 @@
 package downloader
 
 import (
-	"fmt"
 	"github.com/jlaffaye/ftp"
 )
 
-func Hello() {
-	fmt.Println("Hello!")
-}
-
-// Will download a file from an FTP server, specified by addr. Addr should contain a hostname and a port number.
+// DownloadFileAnonymously will download a file from an FTP server, specified by addr. Addr should contain a hostname and a port number.
 // Filepath path and filename of the file to download.
 // The method returns a Response which can be read for the contents of the downloaded file. The Response MUST BE closed
 // after use, otherwise FTP errors might happen.
@@ -17,6 +12,7 @@ func DownloadFileAnonymously(addr, filePath string) *ftp.Response {
 	return DownloadFile(addr, "anonymous", "anonymous", filePath)
 }
 
+// DownloadFile fetches file with credentials
 func DownloadFile(addr, username, password, filePath string) *ftp.Response {
 	con, err := ftp.Connect(addr)
 	if err != nil {
